6663d76a0b0c274b6a40005e857ee4e9182a6a81
Tranquil-M <bpihwhshmh@gmail.com>
2026-01-03 13:34:45 -0800

    fixed package names

--- stow/.config/hypr/conf.d/post-install.sh
+++
@@ -1,8 +0,0 @@
-echo "Applying Hyprland Colorscheme..."
-cd "$HOME"
-if command -v matugen >/dev/null 2>&1; then
-    matugen -q image "Pictures/Wallpapers/White-Cat.png" >/dev/null 2>&1
-fi
-gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
-
-rm -- "$0"
