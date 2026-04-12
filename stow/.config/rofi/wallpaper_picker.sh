b585536a80950b1174bd00264fa5abd417e580df
Tranquil-M <bpihwhshmh@gmail.com>
2026-04-12 14:16:22 -0700

    updated hyprlock screen

--- stow/.config/rofi/wallpaper_picker.sh
+++ stow/.config/rofi/wallpaper_picker.sh
@@ -52,3 +52,4 @@ wall_selection=$(
 [[ -n "$wall_selection" ]] || exit 1
 matugen image "${wall_dir}/${wall_selection}" --source-color-index 0 || matugen image "${wall_dir}/${wall_selection}"
 awww img --resize crop --transition-type center "${wall_dir}/${wall_selection}"
+cp "${wall_dir}/${wall_selection}" ~/Pictures/current_wallpaper.png
