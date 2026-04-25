b740976695db586d5e1dc77feee8384c7d4c1e7f
Tranquil-M <bpihwhshmh@gmail.com>
2026-04-24 18:46:01 -0700

    removed duplicates because that's there for some reason

--- stow/.config/hypr/conf.d/scripts/is_floating.sh
+++
@@ -1,6 +0,0 @@
-IS_FLOATING=$(hyprctl activewindow -j | jq '.floating')
-
-if [ "$IS_FLOATING" == "true" ]; then
-    hyprctl dispatch resizeactive exact 60% 75%
-    hyprctl dispatch centerwindow
-fi
--- stow/.config/swaync/scripts/airplane_mode.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if rfkill list | grep -q "Soft blocked: yes"; then
-  rfkill unblock all
-  notify-send "Airplane mode" "OFF"
-else
-  rfkill block all
-  notify-send "Airplane mode" "ON"
-fi
