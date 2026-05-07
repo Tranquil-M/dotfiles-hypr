e0699a508f3be7ac765cb84c2bf380c956001a9a
Tranquil-M <bpihwhshmh@gmail.com>
2026-05-06 21:30:25 -0700

    added fullscreen screenshot utility

---
+++ dot_config/hypr/conf.d/scripts/executable_screenshot_screen.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SAVE_DIR="$HOME/Pictures/Screenshots"
+mkdir -p "$SAVE_DIR"
+
+FILENAME="$(date +'%m %d %Y : %I:%M:%S').png"
+FILEPATH="$SAVE_DIR/$FILENAME"
+
+grim "$FILEPATH"
+
+satty --filename "$FILEPATH"
+
+wl-copy < "$FILEPATH"
