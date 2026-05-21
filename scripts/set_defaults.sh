d50fd271f457ded04929220dccb3e3fee1961524
Tranquil-M <bpihwhshmh@gmail.com>
2026-05-20 20:00:08 -0700

    updated defaults

--- scripts/set_defaults.sh
+++ scripts/set_defaults.sh
@@ -4,4 +4,5 @@ source "scripts/ui.sh"
 
 info "Setting defaults..."
 substep xdg-mime default org.gnome.Nautilus.desktop inode/directory
+substep sudo systemctl enable --now power-profiles-daemon.service
 success "Set defaults sucessfuly!"
