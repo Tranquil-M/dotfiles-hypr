064977c4b9f82b5a3df3da9d87dca750a192b3c9
Tranquil-M <bpihwhshmh@gmail.com>
2026-05-21 18:47:37 -0700

    opted tlp

--- scripts/set_defaults.sh
+++ scripts/set_defaults.sh
@@ -4,5 +4,6 @@ source "scripts/ui.sh"
 
 info "Setting defaults..."
 substep xdg-mime default org.gnome.Nautilus.desktop inode/directory
-substep sudo systemctl enable --now power-profiles-daemon.service
+substep sudo systemctl enable --now tlp.service
+substep sudo tlp balanced
 success "Set defaults sucessfuly!"
