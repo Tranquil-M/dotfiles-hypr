0a80136f61df497202c80b0753f56c1cb932091f
Tranquil-M <bpihwhshmh@gmail.com>
2026-05-23 00:48:56 -0700

    added plugin file; updated screenshot utility to not auto save

--- config/.config/hypr/modules/keybind_modules/screenshots.lua
+++ config/.config/hypr/modules/keybind_modules/screenshots.lua
@@ -1,6 +1,6 @@
 -- Variables
 local mainMod = "SUPER"
 
--- Scripts
-hl.bind(mainMod .. " + Z",         hl.dsp.exec_cmd("sh ~/.config/hypr/modules/scripts/screenshot_region.sh"))
-hl.bind(mainMod .. " + SHIFT + Z", hl.dsp.exec_cmd("sh ~/.config/hypr/modules/scripts/screenshot_screen.sh"))
+-- Execution
+hl.bind(mainMod .. " + z", hl.dsp.exec_cmd('grim -g "$(slurp)" - | satty -f -'))
+hl.bind(mainMod .. " + SHIFT + Z", hl.dsp.exec_cmd('grim - | satty -f -'))
