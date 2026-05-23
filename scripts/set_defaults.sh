d28919d6b96251b7e9f1c69c2ed9c4b5f0975f96
Tranquil-M <bpihwhshmh@gmail.com>
2026-05-22 21:14:08 -0700

    power settings

--- scripts/set_defaults.sh
+++ scripts/set_defaults.sh
@@ -5,19 +5,6 @@ source "scripts/ui.sh"
 info "Setting defaults..."
 substep xdg-mime default org.gnome.Nautilus.desktop inode/directory
 substep sudo systemctl enable --now sddm.service
-substep sudo systemctl enable --now tlp.service
-substep sudo systemctl enable --now tlp-pd.service
-substep sudo tlp balanced
-substep echo "CPU_DRIVER_OPMODE_ON_AC=active
-CPU_DRIVER_OPMODE_ON_BAT=passive
-CPU_SCALING_GOVERNOR_ON_AC=ondemand
-CPU_SCALING_GOVERNOR_ON_BAT=conservative
-CPU_BOOST_ON_AC=1
-CPU_BOOST_ON_BAT=0
-PLATFORM_PROFILE_ON_AC=perforamce
-PLATFORM_PROFILE_ON_BAT=low-power
-TLP_PROFILE_AC=BAL
-TLP_PROFILE_BAT=SAV
-TLP_AUTO_SWITCH=2" | sudo tee -a /etc/tlp.conf
+substep sudo systemctl enable --now power-profiles-daemon.service
 
 success "Set defaults sucessfuly!"
