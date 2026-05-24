9cbe308bc3c912d3f3b10c71a0eb3717b32844b2
Tranquil-M <bpihwhshmh@gmail.com>
2026-05-23 19:46:49 -0700

    changed kill to close for a gracefull app termination

--- config/.config/hypr/modules/keybind_modules/window_management.lua
+++ config/.config/hypr/modules/keybind_modules/window_management.lua
@@ -16,7 +16,7 @@ hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 -- Actions
-hl.bind(mainMod .. " + W", hl.dsp.window.kill())
+hl.bind(mainMod .. " + W", hl.dsp.window.close())
 hl.bind(mainMod .. " + T", function()
 
 	local monitor = hl.get_active_monitor()
