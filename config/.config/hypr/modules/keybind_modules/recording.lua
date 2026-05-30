40f7789e1d1c3592540002f86e1cfee6c2ecab5a
Tranquil-M <bpihwhshmh@gmail.com>
2026-05-30 00:28:11 -0700

    made recording functional; added sfx for screenshot and record

--- config/.config/hypr/modules/keybind_modules/recording.lua
+++ config/.config/hypr/modules/keybind_modules/recording.lua
@@ -1,5 +1,19 @@
 -- Variables
 local mainMod = "SUPER"
 
+-- Helper Functions
+local record_desktop_audio = [[
+if pgrep -x wf-recorder >/dev/null; then
+    hyprcap rec monitor:active -wN
+    mpv ~/.config/hypr/sfx/end_record.caf
+    while pgrep -x wf-recorder >/dev/null; do sleep 0.1; done
+    mpv ~/Videos/Captures/"$(exa -s=mod -r -1 ~/Videos/Captures | head -n1)"
+else
+    mpv ~/.config/hypr/sfx/begin_record.caf
+    pactl set-default-source easyeffects_sink.monitor
+    hyprcap rec monitor:active -wN
+fi
+]]
+
 -- Execution
-hl.bind(mainMod .. " + ALT + z", hl.dsp.exec_cmd('pactl set-default-source "$(pactl get-default-sink).monitor"; hyprcap rec monitor:active --audio -wc'))
+hl.bind(mainMod .. " + ALT + z", hl.dsp.exec_cmd(record_desktop_audio))
--- config/.config/hypr/modules/keybind_modules/screenshots.lua
+++ config/.config/hypr/modules/keybind_modules/screenshots.lua
@@ -2,6 +2,6 @@
 local mainMod = "SUPER"
 
 -- Execution
-hl.bind(mainMod .. " + z", hl.dsp.exec_cmd('hyprcap shot region -WFNcrz - | satty --filename -'))
+hl.bind(mainMod .. " + z", hl.dsp.exec_cmd('hyprcap shot region -WFNcz -o ~/Pictures/Screenshots && mpv $HOME/.config/hypr/sfx/Grab.aif && satty --filename ~/Pictures/Screenshots/"$(exa -s=mod -r -1 ~/Pictures/Screenshots | head -n1)"'))
 
-hl.bind(mainMod .. " + SHIFT + z", hl.dsp.exec_cmd('hyprcap shot monitor:active -WFNcr - | satty --filename -'))
+hl.bind(mainMod .. " + SHIFT + z", hl.dsp.exec_cmd('hyprcap shot monitor -WFNc -o ~/Pictures/Screenshots && mpv $HOME/.config/hypr/sfx/Grab.aif && satty --filename ~/Pictures/Screenshots/"$(exa -s=mod -r -1 ~/Pictures/Screenshots | head -n1)"'))
