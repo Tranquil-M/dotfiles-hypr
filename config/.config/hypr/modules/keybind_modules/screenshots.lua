31817e647aeb91d250e706141b56ac878f963d96
Tranquil-M <bpihwhshmh@gmail.com>
2026-05-30 00:35:02 -0700

    restructured for readability

--- config/.config/hypr/modules/keybind_modules/screenshots.lua
+++ config/.config/hypr/modules/keybind_modules/screenshots.lua
@@ -1,7 +1,20 @@
 -- Variables
 local mainMod = "SUPER"
 
--- Execution
-hl.bind(mainMod .. " + z", hl.dsp.exec_cmd('hyprcap shot region -WFNcz -o ~/Pictures/Screenshots && mpv $HOME/.config/hypr/sfx/Grab.aif && satty --filename ~/Pictures/Screenshots/"$(exa -s=mod -r -1 ~/Pictures/Screenshots | head -n1)"'))
+-- Helper Functions
+local regional_screenshot = table.concat({
+      "hyprcap shot region -WFNcz -o /tmp/screenies/",
+      "mpv $HOME/.config/hypr/sfx/Grab.aif",
+      'satty --filename /tmp/screenies/"$(exa -s=mod -r -1 /tmp/screenies | head -n1)"',
+}, " && ")
 
-hl.bind(mainMod .. " + SHIFT + z", hl.dsp.exec_cmd('hyprcap shot monitor -WFNc -o ~/Pictures/Screenshots && mpv $HOME/.config/hypr/sfx/Grab.aif && satty --filename ~/Pictures/Screenshots/"$(exa -s=mod -r -1 ~/Pictures/Screenshots | head -n1)"'))
+local monitor_screenshot = table.concat({
+      "hyprcap shot monitor -WFNcz -o /tmp/screenies/",
+      "mpv $HOME/.config/hypr/sfx/Grab.aif",
+      'satty --filename /tmp/screenies/"$(exa -s=mod -r -1 /tmp/screenies | head -n1)"',
+}, " && ")
+
+-- Execution
+hl.bind(mainMod .. " + z", hl.dsp.exec_cmd(regional_screenshot))
+
+hl.bind(mainMod .. " + SHIFT + z", hl.dsp.exec_cmd(monitor_screenshot))
