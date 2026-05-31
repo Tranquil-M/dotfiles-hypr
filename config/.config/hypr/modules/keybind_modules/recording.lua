7b3010ba1542766727dfaf11389b03ae7013b273
Tranquil-M <bpihwhshmh@gmail.com>
2026-05-30 18:47:49 -0700

    added regional recording

--- config/.config/hypr/modules/keybind_modules/recording.lua
+++ config/.config/hypr/modules/keybind_modules/recording.lua
@@ -15,5 +15,19 @@ else
 fi
 ]]
 
+local record_regional_audio = [[
+if pgrep -x wf-recorder >/dev/null; then
+    hyprcap rec region -wN
+    mpv ~/.config/hypr/sfx/end_record.caf
+    while pgrep -x wf-recorder >/dev/null; do sleep 0.1; done
+    mpv ~/Videos/Captures/"$(exa -s=mod -r -1 ~/Videos/Captures | head -n1)"
+else
+    mpv ~/.config/hypr/sfx/begin_record.caf
+    pactl set-default-source easyeffects_sink.monitor
+    hyprcap rec region -wN
+fi
+]]
+
 -- Execution
 hl.bind(mainMod .. " + ALT + z", hl.dsp.exec_cmd(record_desktop_audio))
+hl.bind(mainMod .. " + CTRL + z", hl.dsp.exec_cmd(record_regional_audio))
