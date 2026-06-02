6ed60fb59932e58983be35c64ee0f56c54fd61a8
Tranquil-M <bpihwhshmh@gmail.com>
2026-06-01 18:50:56 -0700

    swapped backt odefault wallpaper selector

--- config/.config/hypr/modules/keybind_modules/applications.lua
+++ config/.config/hypr/modules/keybind_modules/applications.lua
@@ -6,7 +6,7 @@ local ipc = "qs -c noctalia-shell ipc call"
 hl.bind(mainMod .. " + RETURN",    hl.dsp.exec_cmd("kitty"))
 
 -- Noctalia
-hl.bind(mainMod .. " + E",         hl.dsp.exec_cmd(ipc .. " plugin:wallcards toggle"))
+hl.bind(mainMod .. " + E",         hl.dsp.exec_cmd(ipc .. " wallpaper toggle"))
 hl.bind(mainMod .. " + X",     hl.dsp.exec_cmd(ipc .. " launcher emoji"))
 hl.bind(mainMod .. " + V",     hl.dsp.exec_cmd(ipc .. " launcher clipboard"))
 hl.bind(mainMod .. " + M",     hl.dsp.exec_cmd(ipc .. " media toggle"))
