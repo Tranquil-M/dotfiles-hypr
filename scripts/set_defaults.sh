423b7591c7f6ecbebcf25a5561ec124b14ac0c94
Tranquil-M <bpihwhshmh@gmail.com>
2026-06-03 22:44:33 -0700

    BONGO CCAT

--- scripts/set_defaults.sh
+++ scripts/set_defaults.sh
@@ -6,5 +6,6 @@ info "Setting defaults..."
 substep xdg-mime default org.gnome.Nautilus.desktop inode/directory
 substep sudo systemctl enable --now sddm.service
 substep sudo systemctl enable --now power-profiles-daemon.service
+substep sudo usermod -a -G input $USER
 
 success "Set defaults sucessfuly!"
