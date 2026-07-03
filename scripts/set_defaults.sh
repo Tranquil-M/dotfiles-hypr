3dc5d73ea5460affb5ca2b670115b9a41e5c309e
Tranquil-M <bpihwhshmh@gmail.com>
2026-07-03 15:08:14 -0700

    fixed default settings

--- scripts/set_defaults.sh
+++ scripts/set_defaults.sh
@@ -3,9 +3,12 @@
 source "scripts/ui.sh"
 
 info "Setting defaults..."
-substep xdg-mime default org.gnome.Nautilus.desktop inode/directory
-substep sudo systemctl enable --now sddm.service
-substep sudo systemctl enable --now power-profiles-daemon.service
-substep sudo usermod -a -G input $USER
+
+xdg-mime default org.gnome.Nautilus.desktop inode/directory
+xdg-mime default firefox.desktop x-scheme-handler/http
+xdg-mime default firefox.desktop x-scheme-handler/https
+sudo systemctl enable --now sddm.service
+sudo systemctl enable --now power-profiles-daemon.service
+sudo usermod -a -G input $USER
 
 success "Set defaults sucessfuly!"
