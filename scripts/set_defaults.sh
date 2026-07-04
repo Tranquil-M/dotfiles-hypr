de2a4dd011af3a4a3f907292c86003affe1aa74e
Tranquil-M <bpihwhshmh@gmail.com>
2026-07-03 17:35:40 -0700

    settings changes

--- scripts/set_defaults.sh
+++ scripts/set_defaults.sh
@@ -11,4 +11,10 @@ sudo systemctl enable --now sddm.service
 sudo systemctl enable --now power-profiles-daemon.service
 sudo usermod -a -G input $USER
 
+for mime in text/plain text/markdown text/x-tex text/html text/css text/javascript \
+            application/json application/javascript application/xml application/x-yaml \
+            text/x-c text/x-c++src text/x-python text/x-shellscript; do
+    xdg-mime default nvim.desktop "$mime"
+done
+
 success "Set defaults sucessfuly!"
