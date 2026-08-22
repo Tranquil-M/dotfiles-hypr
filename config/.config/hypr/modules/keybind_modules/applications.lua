8914965e0a077ea85cb37a6b608f39314390aa45
Tranquil-M <bpihwhshmh@gmail.com>
2026-08-22 15:54:08 -0700

    updated application keybindings

--- config/.config/hypr/modules/keybind_modules/applications.lua
+++ config/.config/hypr/modules/keybind_modules/applications.lua
@@ -1,13 +1,13 @@
 -- Variables
 local mainMod = "SUPER"
-local ipc = "qs -c noctalia-shell ipc call"
+local ipc = "noctalia msg"
 
 -- Apps
 hl.bind(mainMod .. " + RETURN",    hl.dsp.exec_cmd("kitty"))
 
 -- Noctalia
-hl.bind(mainMod .. " + E",         hl.dsp.exec_cmd(ipc .. " wallpaper toggle"))
-hl.bind(mainMod .. " + X",     hl.dsp.exec_cmd(ipc .. " launcher emoji"))
-hl.bind(mainMod .. " + V",     hl.dsp.exec_cmd(ipc .. " plugin:clipboard toggle"))
-hl.bind(mainMod .. " + M",     hl.dsp.exec_cmd(ipc .. " media toggle"))
-hl.bind("ALT + Tab", hl.dsp.exec_cmd(ipc .. " launcher windows"))
+hl.bind(mainMod .. " + E",         hl.dsp.exec_cmd(ipc .. " panel-toggle wallpaper"))
+hl.bind(mainMod .. " + X",     hl.dsp.exec_cmd(ipc .. " panel-toggle launcher /emo "))
+hl.bind(mainMod .. " + V",     hl.dsp.exec_cmd(ipc .. " panel-toggle clipboard"))
+hl.bind(mainMod .. " + M",     hl.dsp.exec_cmd(ipc .. " panel-toggle control-center media"))
+hl.bind("ALT + Tab", hl.dsp.exec_cmd(ipc .. " window-switcher"))
