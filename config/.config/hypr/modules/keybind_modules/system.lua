ca4fbdc7b871d16480eb5e224929360e7bf51000
Tranquil-M <bpihwhshmh@gmail.com>
2026-08-22 16:07:25 -0700

    updated system

--- config/.config/hypr/modules/keybind_modules/system.lua
+++ config/.config/hypr/modules/keybind_modules/system.lua
@@ -1,12 +1,12 @@
 -- Variables
 local mainMod = "SUPER"
-local ipc = "qs -c noctalia-shell ipc call"
+local ipc = "noctalia msg"
 
 -- Cosmetic
-hl.bind(mainMod .. " + A", hl.dsp.exec_cmd(ipc .. " bar toggle"))
+hl.bind(mainMod .. " + A", hl.dsp.exec_cmd(ipc .. " bar-toggle"))
 
 -- Laptop
 hl.bind("switch:on:Lid Switch",
-    hl.dsp.exec_cmd(ipc .. " lockScreen lock & systemctl suspend"),
+    hl.dsp.exec_cmd(ipc .. " session lock-and-suspend"),
     { locked = true }
 )
