02225138e0853791cf29176401ef54f280924aef
Tranquil-M <bpihwhshmh@gmail.com>
2026-08-23 15:50:41 -0700

    added settings window open window rule

--- config/.config/hypr/modules/windows.lua
+++ config/.config/hypr/modules/windows.lua
@@ -81,3 +81,13 @@ hl.window_rule({
 
   animation = "slide right",
 })
+
+hl.window_rule({
+    match = {
+        title = "Noctalia Settings",
+    },
+
+    float = true,
+    center = true,
+    size = { "monitor_w * 0.5", "monitor_h * 0.8" },
+})
