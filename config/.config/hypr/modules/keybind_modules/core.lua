f95e847220fee7d85a8b65c44c7a3ac8e260187c
Tranquil-M <bpihwhshmh@gmail.com>
2026-08-22 22:43:38 -0700

    updated battery binding

--- config/.config/hypr/modules/keybind_modules/core.lua
+++ config/.config/hypr/modules/keybind_modules/core.lua
@@ -7,7 +7,7 @@ hl.bind(mainMod .. " + SPACE", hl.dsp.exec_cmd(ipc .. " panel-toggle launcher"))
 hl.bind(mainMod .. " + C",     hl.dsp.exec_cmd(ipc .. " panel-toggle control-center"))
 hl.bind(mainMod .. " + S",     hl.dsp.exec_cmd(ipc .. " settings-open"))
 hl.bind(mainMod .. " + N",     hl.dsp.exec_cmd(ipc .. " panel-toggle control-center notifications"))
-hl.bind(mainMod .. " + B",     hl.dsp.exec_cmd(ipc .. " panel-toggle piero-93/battery-power-management:panel"))
+hl.bind(mainMod .. " + B",     hl.dsp.exec_cmd(ipc .. " panel-toggle control-center power"))
 
 -- Logout
 hl.bind(mainMod .. " + BACKSPACE", hl.dsp.exec_cmd(ipc .. " panel-toggle session"))
